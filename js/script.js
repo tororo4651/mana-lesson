277fe7beeedc4a822499c25ed857c7218e9ce0d9
tororo4651 <>
2024-08-14 15:12:22 +0900

    更新

--- js/script.js
+++ js/script.js
@@ -1,52 +1,15 @@
+const heading = document.querySelector('#heading');
 
-const juices = [
-  {
-    name: 'イチゴ',
-    img: 'strawberry.jpg',
-    price: 450
-  },
-  {
-    name: 'ライム',
-    img: 'lime.jpg',
-    price: 400
-  },
-  {
-    name: 'マンゴー',
-    img: 'mango.jpg',
-    price: 500
-  },
-  {
-    name: 'レモン',
-    img: 'lemon.jpg',
-    price: 400
-  },
-  {
-    name: 'イチジク',
-    img: 'fig.jpg',
-    price: 500
-  },
-  {
-    name: 'リンゴ',
-    img: 'apple.jpg',
-    price: 400
-  }
-];
+const keyframes = {
+  backgroundPosition: ['right top', 'left top'],
+  color: ['rgb(255 255 255 / 0%)', 'rgb(255 255 255 / 100%)']
+};
+
+const options = {
+  duration: 1000,
+  easing: 'ease',
+  fill: 'forwards'
+};
 
 
-const menu = document.querySelector('.menu');
-
-
-juices.forEach((juice, index, array) => {
-  const { name, img, price } = juice;
-
-  const content =
-   `<div class="menu__item">
-      <img class="menu__itemImage" src="./images/${img}" alt="">
-
-      <p class="menu__itemName">${name}</p>
-
-      <p class="menu__itemPrice">${price}円</p>
-    </div>`;
-
-  menu.insertAdjacentHTML('beforeend', content);
-});
+heading.animate(keyframes, options);
