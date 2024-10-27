67547fbe8ad4265dff5c2100d94c7144c8f39f89
tororo4651 <>
2024-10-27 19:08:55 +0900

    更新

--- js/script.js
+++ js/script.js
@@ -1,20 +1,57 @@
-const texts = document.getElementsByClassName('text');
+const slideLeft = (entries, obs) => {
 
-const images = document.getElementsByTagName('img');
-const images2 = document.querySelectorAll('img');
+  for (let i = 0; i < entries.length; i++) {
+    if (entries[i].isIntersecting) {
+      entries[i].target.animate(
+        {
+          translate: [0, '100%']
+        },
+        {
+          duration: 2000,
+          pseudoElement: '::after',
+          easing: 'ease',
+          fill: 'forwards'
+        }
+      );
+
+      obs.unobserve(entries[i].target);
+    }
+  }
 
 
-images.forEach((image, index, array) => {
-  image.addEventListener('mouseover', function() {
-    texts[0].innerText = this.alt;
 
-    this.animate(
-      {
-        opacity: [0, 1]
-      },
-      {
-        duration: 500
-      }
-    );
-  }, false);
-});
+  // entries.forEach((entry, index, array) => {
+  //   // console.log(entry);
+  //   if (entry.isIntersecting) {
+  //     entry.target.animate(
+  //       {
+  //         translate: [0, '100%']
+  //       },
+  //       {
+  //         duration: 2000,
+  //         pseudoElement: '::after',
+  //         easing: 'ease',
+  //         fill: 'forwards'
+  //       }
+  //     );
+
+  //     obs.unobserve(entry.target);
+  //   }
+  // });
+
+};
+
+
+const slideLeftObserver = new IntersectionObserver(slideLeft);
+
+
+const slideLeftElements = document.getElementsByClassName('scroll');
+
+for (let i = 0; i < slideLeftElements.length; i++) {
+  slideLeftObserver.observe(slideLeftElements[i]);
+}
+
+
+// slideLeftElements.forEach((slideLeftElement, index, array) => {
+//   slideLeftObserver.observe(slideLeftElement);
+// });
